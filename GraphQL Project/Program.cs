dd24a30f4e2bb5fac6ce6732a93f9c5b6392781b
ammad khan <ammad172@gmail.com>
2024-08-21 23:17:07 +0500

    Add project files.

---
+++ GraphQL Project/Program.cs
@@ -0,0 +1,14 @@
+using GraphQL_Project.Query_class;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL();
+
+app.Run();
+
---
+++ GraphQL Project/Query_class/Query.cs
@@ -0,0 +1,25 @@
+﻿namespace GraphQL_Project.Query_class
+{
+    public class Query
+    {
+        public Book GetBook() =>
+         new Book
+         {
+             Title = "C# in depth.",
+             Author = new Author
+             {
+                 Name = "Jon Skeet"
+             }
+         };
+
+
+        Author author = new Author() { Name = "Adnan" };
+        public IEnumerable<Book> GetBooks() => new List<Book>() {
+       
+
+            new Book {Title = "Idun" , Author = author},
+            new Book {Title = "Tippla" , Author = author},
+
+            };
+    }
+}
